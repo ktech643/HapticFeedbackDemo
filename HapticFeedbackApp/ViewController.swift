dafc951dd65a647fd7934190514d2766dc0b6f14
KTC <ktech643@gmail.com>
2024-11-16 18:25:37 +0500

    Demo Work

---
+++ HapticFeedbackApp/ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  HapticFeedbackApp
+//
+//  Created by Jahan on 16/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var dot: UIView!
+    private let dotSize: CGFloat = 50.0 // Size of the dot
+    private let maxDistance: CGFloat = 1000.0 // Maximum distance for feedback
+    private var hapticGenerator: UIImpactFeedbackGenerator?
+    
+    private var screenBounds: CGRect {
+        return self.view.bounds
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+        // Create the dot
+        dot = UIView(frame: CGRect(x: 0, y: 0, width: dotSize, height: dotSize))
+        dot.backgroundColor = .red
+        dot.layer.cornerRadius = dotSize / 2
+        self.view.addSubview(dot)
+        
+        placeDotRandomly()
+        addTouchGesture()
+    }
+    
+    // Places the dot at a random position on the screen
+    private func placeDotRandomly() {
+        let maxX = screenBounds.width - dotSize
+        let maxY = screenBounds.height - dotSize
+        let randomX = CGFloat.random(in: 0...maxX)
+        let randomY = CGFloat.random(in: 0...maxY)
+        dot.center = CGPoint(x: randomX, y: randomY)
+    }
+    
+    private func addTouchGesture() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
+        self.view.addGestureRecognizer(panGesture)
+    }
+    
+    @objc private func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
+        let touchPoint = gesture.location(in: self.view)
+        
+        switch gesture.state {
+        case .changed:
+            provideProximityFeedback(touchPoint: touchPoint)
+        case .ended:
+            // Reset the dot if the user reached it
+            if isTouchOnDot(touchPoint: touchPoint) {
+                triggerHapticClick()
+                placeDotRandomly()
+            }
+        default:
+            break
+        }
+    }
+    
+    // Checks if the user's touch is on the dot
+    private func isTouchOnDot(touchPoint: CGPoint) -> Bool {
+        let distance = hypot(dot.center.x - touchPoint.x, dot.center.y - touchPoint.y)
+        return distance <= dotSize / 2
+    }
+    
+    private func provideProximityFeedback(touchPoint: CGPoint) {
+        let distance = hypot(dot.center.x - touchPoint.x, dot.center.y - touchPoint.y)
+        
+        // Calculate intensity: closer to the dot results in higher intensity
+        let intensity = max(0, 1.0 - (distance / maxDistance))
+        
+        if intensity > 0 { // Only trigger feedback if within max distance
+            if hapticGenerator == nil {
+                hapticGenerator = UIImpactFeedbackGenerator(style: .heavy)
+                hapticGenerator?.prepare()
+            }
+            hapticGenerator?.impactOccurred(intensity: intensity)
+        }
+    }
+    
+    private func triggerHapticClick() {
+        let notificationGenerator = UINotificationFeedbackGenerator()
+        notificationGenerator.notificationOccurred(.success)
+        hapticGenerator = nil // Reset haptic generator after a successful touch
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# HapticFeedbackDemo
+# Proximity-Based Haptic Feedback App
+
+## Overview
+This app provides haptic feedback that increases in intensity as your finger gets closer to a randomly placed dot on the screen. Once you touch the dot, a satisfying haptic click is triggered, and the dot resets to a new location.
+
+## Features
+- Dynamic dot placement within screen bounds.
+- Proximity-based vibration feedback.
+- Supports all screen sizes.
+
+## How to Run
+1. Open the project in Xcode.
+2. Select an iPhone simulator or device with haptic feedback.
+3. Run the app.
+
+## Assumptions
+- The app assumes the device supports `UIImpactFeedbackGenerator` for haptics.
